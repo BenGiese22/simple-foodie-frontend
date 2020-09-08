3f2a6baaa0a786b115747de46dc7b7bc66703e8f
Ben <bengiese22@gmail.com>
2020-09-07 21:57:08 -0600

    Add grid to recipe page

--- src/pages/recipe/recipe.js
+++ src/pages/recipe/recipe.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography'
 import RecipeApi from '../../service/recipe-api'
 import LoadingSpinner from 'react-loader-spinner'
 
+import Grid from '@material-ui/core/Grid'
+
 import './recipe.css'
 
 const BASE_URL = 'http://localhost:3000';
@@ -55,30 +57,38 @@ class Recipe extends Component {
         if (this.state.recipe === null) {
             return (
                 <div id='recipe'>
-                    <LoadingSpinner type="ThreeDots" color="black" height={80} width={80}/>
+                    <Grid container spacing={0}>
+                        <Grid item xs={12}>
+                            <LoadingSpinner type="ThreeDots" color="black" height={80} width={80}/>
+                        </Grid>
+                    </Grid>
                 </div>
             )
         } else {
             return (
                 <div id='recipe'>
-                    <Typography align="center" variant="h4" gutterBottom>
-                        {this.state.title}
-                    </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        <a href={BASE_URL}>Home</a>
-                    </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        <a href={this.state.link}>External Link</a>
-                    </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        Ingredients
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>{this.state.ingredients}</Typography>
-                    <Typography variant="h5" gutterBottom>
-                        Directions
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>{this.state.directions}</Typography>
-                    <Typography variant="body2" align="right" gutterBottom>Source: {this.state.source}</Typography>
+                    <Grid container spacing={0}>
+                        <Grid item xs={12}>
+                            <Typography align="center" variant="h4" gutterBottom>
+                                {this.state.title}
+                            </Typography>
+                            <Typography variant="h5" gutterBottom>
+                                <a href={BASE_URL}>Home</a>
+                            </Typography>
+                            <Typography variant="h5" gutterBottom>
+                                <a href={this.state.link}>External Link</a>
+                            </Typography>
+                            <Typography variant="h5" gutterBottom>
+                                Ingredients
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>{this.state.ingredients}</Typography>
+                            <Typography variant="h5" gutterBottom>
+                                Directions
+                            </Typography>
+                            <Typography variant="body1" gutterBottom>{this.state.directions}</Typography>
+                            <Typography variant="body2" align="right" gutterBottom>Source: {this.state.source}</Typography>
+                        </Grid>
+                    </Grid>
                 </div>
             )
         }
